0a9973c45b220e9a0b23933cce1798176f9dc73f
naoloui <naomi.laufer16@gmail.com>
2025-08-14 17:20:17 +0200

    Modifications on seven files

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import Weather from "./Weather";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <h1>Weather App React.js</h1>
+        <Weather defaultCity="Madrid" />
+
+        <footer>
+          Coded by
+          <a
+            href="https://github.com/naoloui/weather-app-reactjs"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Naomi L
+          </a>
+          and is open-sourced on
+          <a
+            href="https://github.com/naoloui/weather-app-reactjs"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            GitHub
+          </a>
+          and hosted on
+          <a
+            href="https://app.netlify.com/projects/meteoapp-reactjs-naoloui/overview"
+            target="_blank"
+          >
+            Netlify
+          </a>
+          .
+        </footer>
+      </div>
+    </div>
+  );
+}
